188c310e00cbbdc1dee8288ae112a2b9d6423c5c
Manuel Herzog <manuel.herzog@esentri.com>
2022-10-17 11:58:34 +0200

    disabled Autoshift and added more shift keys

--- keyboards/avalanche/v4/keymaps/sepharim/keymap.c
+++ keyboards/avalanche/v4/keymaps/sepharim/keymap.c
@@ -27,10 +27,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV]  = MY_LAYOUT(
                         KC_PGUP , KC_BSPC , KC_UP   , KC_DEL  , KC_PGDN ,           xxxxxxx , KC_7    , KC_8    , KC_9    , xxxxxxx ,
                         KC_HOME , KC_LEFT , KC_DOWN , KC_RGHT , KC_END  ,           xxxxxxx , MY_4    , MY_5    , MY_6    , MY_Nxxx ,
-                        KC_ESC  , KC_TAB  , KC_INS  , KC_ENT  , xxxxxxx ,           KC_0    , KC_1    , KC_2    , KC_3    , xxxxxxx ,
-                                                      _______ , _______ ,           KC_LSFT , _______),
+                        KC_ESC  , KC_TAB  , KC_INS  , KC_ENT  , KC_LSFT ,           KC_0    , KC_1    , KC_2    , KC_3    , xxxxxxx ,
+                                                      _______ , _______ ,           _______ , _______),
     [_FN]   = MY_LAYOUT(
-                        _______ , _______ , _______ , _______ , _______ ,           xxxxxxx , KC_F7   , KC_F8   , KC_F9   , KC_F10  ,
+                        KC_LCTL , KC_LALT , KC_LGUI , KC_LSFT , _______ ,           xxxxxxx , KC_F7   , KC_F8   , KC_F9   , KC_F10  ,
                         OSM(MOD_LCTL) , OSM(MOD_LALT) , OSM(MOD_LGUI) , OSM(MOD_LSFT) , _______ ,           xxxxxxx , KC_F4   , KC_F5   , KC_F6   , KC_F11  ,
                         _______ , _______ , _______ , _______ , _______ ,           xxxxxxx , KC_F1   , KC_F2   , KC_F3   , KC_F12  ,
                                                       _______ , _______ ,           _______ , _______),
